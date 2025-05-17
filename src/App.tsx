6ac53ae41ff9dd6d2e2e003476793402a82a385c
Jaydoo88 <jasondousharm@gmail.com>
2025-05-17 15:00:25 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,25 +24,28 @@ function App() {
             <img src={Logo} alt="FallenClassmate Logo" />
           </div>
 
+  <div className="school-badge-wrapper">
   <div className="school-badge">
-  <div className="cap-row">
-    <span className="fade-1 offset-2">🎓</span>
-    <span className="fade-2 offset-1">🎓</span>
-    <span className="fade-3 offset-0">🎓</span>
-    <span className="fade-4 offset-1">🎓</span>
-    <span className="fade-5 offset-2">🎓</span>
-    <span className="fade-6 offset-1">🎓</span>
-    <span className="fade-7 offset-0">🎓</span>
-    <span className="fade-8 offset-1">🎓</span>
-    <span className="fade-9 offset-2">🎓</span>
-    <span className="fade-10 offset-1">🎓</span>
-    <span className="fade-11 offset-0">🎓</span>
-    <span className="fade-12 offset-1">🎓</span>
-    <span className="fade-13 offset-2">🎓</span>
-    <span className="fade-14 offset-1">🎓</span>
-    <span className="fade-15 offset-0">🎓</span>
+    <div className="cap-row">
+      {/* 15 caps with fade classes as before */}
+      <span className="fade-1 offset-2">🎓</span>
+      <span className="fade-2 offset-1">🎓</span>
+      <span className="fade-3 offset-0">🎓</span>
+      <span className="fade-4 offset-1">🎓</span>
+      <span className="fade-5 offset-2">🎓</span>
+      <span className="fade-6 offset-1">🎓</span>
+      <span className="fade-7 offset-0">🎓</span>
+      <span className="fade-8 offset-1">🎓</span>
+      <span className="fade-9 offset-2">🎓</span>
+      <span className="fade-10 offset-1">🎓</span>
+      <span className="fade-11 offset-0">🎓</span>
+      <span className="fade-12 offset-1">🎓</span>
+      <span className="fade-13 offset-2">🎓</span>
+      <span className="fade-14 offset-1">🎓</span>
+      <span className="fade-15 offset-0">🎓</span>
+    </div>
+    <small className="badge-subtitle">Honoring Every School, Every Era</small>
   </div>
-  <small className="badge-subtitle">Honoring Every School, Every Era</small>
 </div>
 
           <div className="search-container">
