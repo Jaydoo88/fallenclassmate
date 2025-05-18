cc766b0fdd3ffad053638ff7ada7509c80560c4b
Jaydoo88 <jasondousharm@gmail.com>
2025-05-18 09:22:35 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import SchoolsPage from './SchoolsPage';
+import USASchoolsPage from './USASchoolsPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/schools" element={<SchoolsPage />} />
+      <Route path="/schools/usa" element={<USASchoolsPage />} />
     </Routes>
   );
 }
