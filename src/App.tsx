9b3eddf2d96ebeda0ecfa96711eb90ce4496228b
Jaydoo88 <jasondousharm@gmail.com>
2025-05-19 15:59:11 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from './HomePage';
 import SchoolsPage from './SchoolsPage';
 import USASchoolsPage from './USASchoolsPage';
 import OverseasSchoolsPage from './OverseasSchoolsPage';
+import ResourcesPage from './ResourcesPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/schools" element={<SchoolsPage />} />
       <Route path="/schools/usa" element={<USASchoolsPage />} />
       <Route path="/schools/overseas" element={<OverseasSchoolsPage />} />
+      <Route path="/resources" element={<ResourcesPage />} />
     </Routes>
   );
 }
