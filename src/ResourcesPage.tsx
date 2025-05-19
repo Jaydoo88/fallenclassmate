dcd41d289bb1fe3c7b717b090e61ba53c33b004a
Jaydoo88 <jasondousharm@gmail.com>
2025-05-19 16:07:03 -0700

    Update ResourcesPage.tsx

--- src/ResourcesPage.tsx
+++ src/ResourcesPage.tsx
@@ -1,7 +1,5 @@
 import './ResourcesPage.css';
 import Logo from './assets/logo.png';
-import UsaMap from './assets/usa_map.png';
-import AsiaMap from './assets/asia_map.jpg';
 
 function ResourcesPage() {
   return (
@@ -21,33 +19,9 @@ function ResourcesPage() {
         <img src={Logo} alt="FallenClassmate Logo" className="resources-logo-img" />
       </section>
 
-      <section className="resources-map-section">
-        <div className="map-item">
-          <h3>USA Grief & Tribute Resources</h3>
-          <a href="/resources/usa">
-            <img src={UsaMap} alt="USA Resources Map" />
-          </a>
-        </div>
-        <div className="map-item">
-          <h3>Overseas & Global Resources</h3>
-          <a href="/resources/overseas">
-            <img src={AsiaMap} alt="Overseas Resources Map" />
-          </a>
-        </div>
-      </section>
-
-      <section className="resources-featured">
-        <h2>Featured Resources</h2>
-        <div className="resources-listing">
-          {/* Placeholder for future content */}
-        </div>
-      </section>
-
-      <section className="resources-spotlight">
-        <h2>Did You Know?</h2>
-        <div className="spotlight-box">
-          <p>Helpful information and stories will appear here soon.</p>
-        </div>
+      <section className="resources-header-section">
+        <h2 className="resources-header-title">Support & Healing Resources</h2>
+        <p className="resources-header-subtext">Tools, guides, and information to help honor and remember.</p>
       </section>
 
       <footer className="resources-footer">
