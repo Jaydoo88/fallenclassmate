79db78f37feb773989da280ccfa80a975e420b58
Jaydoo88 <jasondousharm@gmail.com>
2025-06-01 15:06:01 -0700

    Update SchoolsPage.tsx

--- src/SchoolsPage.tsx
+++ src/SchoolsPage.tsx
@@ -39,6 +39,7 @@ function SchoolsPage() {
           <a href="/plans">Plans & Features</a>
           <a href="/about">About</a>
           <a href="/contact">Contact</a>
+          <a href="/add-school">Add a School</a>
         </nav>
       </header>
 
