30ae2fec867d7ec7a47492fd5c5506345287ec49
Jaydoo88 <jasondousharm@gmail.com>
2025-06-01 22:00:42 -0700

    Create TestForm.tsx

---
+++ src/TestForm.tsx
@@ -0,0 +1,10 @@
+function TestForm() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h2>This is a working test component!</h2>
+      <p>If you see this, routing and rendering works fine.</p>
+    </div>
+  );
+}
+
+export default TestForm;
